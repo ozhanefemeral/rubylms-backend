57e449823e24ef1b0f90226cefd8efc44e300d7b
Özhan Efe Meral <ozhanefemeral@gmail.com>
2020-06-28 14:20:45 +0300

    new build and changed mongoURI

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://ozhanefemeral:ZhEYTQMr7YbZOU2O@cluster0-jzqse.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    mongoURI: 'mongodb+srv://ozhanefemeral:ZhEYTQMr7YbZOU2O@cluster0-jzqse.mongodb.net/ruby?retryWrites=true&w=majority',
 }
