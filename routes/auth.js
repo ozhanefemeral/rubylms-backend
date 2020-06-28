f88126f415b708553a5c88617c71301ee4f9777f
Özhan Efe Meral <ozhanefemeral@gmail.com>
2020-06-28 15:54:21 +0300

    changed login response

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ route.post('/login/student', (req, res) => {
             bcrypt.compare(password, student.password, (err, same) => {
                 if (same) {
                     jwt.sign({ _id: student._id }, '5489d9845m', (err, token) => {
-                        res.json([token, student]);
+                        res.json({ token, studentId: student._id });
                     });
                 } else {
                     res.sendStatus(400);
@@ -32,7 +32,7 @@ route.post('/login/teacher', (req, res) => {
         .then(teacher => {
             bcrypt.compare(password, teacher.password, (err, same) => {
                 if (same) {
-                    jwt.sign({ _id: teacher._id }, '5489d9845m', (err, token) => {
+                    jwt.sign({ token, teacherId: teacher._id }, '5489d9845m', (err, token) => {
                         res.json({
                             token
                         });
